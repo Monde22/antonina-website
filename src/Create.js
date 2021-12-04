9a0ae962050a32ceb4809510ab160cfd3fa9f22c
Monde22 <95472826+Monde22@users.noreply.github.com>
2021-12-04 20:00:59 +0800

    Create.js

--- src/Create.js
+++ src/Create.js
@@ -16,7 +16,7 @@ import { useHistory } from 'react-router-dom';
 
         setIsPending(true);
 
-        fetch('https://antonina-line-website.herokuapp.com/', {
+        fetch('https://antonina-line-website.herokuapp.com', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(list)
