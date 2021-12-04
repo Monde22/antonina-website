3833243b47cad6367e957ce927373b265a7f4047
Monde22 <95472826+Monde22@users.noreply.github.com>
2021-12-04 20:15:24 +0800

    Create.js

--- src/Create.js
+++ src/Create.js
@@ -19,7 +19,7 @@ import { useHistory } from 'react-router-dom';
         fetch('https://antonina-line-website.herokuapp.com', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(list)
+            body: JSON.parse(list)
         }).then(() => {
             console.log('New Passenger Added');
             setIsPending(false);
